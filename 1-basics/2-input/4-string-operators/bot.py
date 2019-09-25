702736da6fc631bdee83f0fa7ccf9a2b7ae25d03
EdsterF <mr.eduardo.franco@gmail.com>
2019-09-25 06:51:20 +0000

    try1

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,30 @@
+#String Operators
+
+print("Please enter the number of lives.")
+lives = input()
+print("")
+
+print("Please enter the energy level.")
+energy = input()
+print("")
+
+print("Please enter the shield level.")
+shield = input()
+print("")
+
+#Symbols
+#l = a
+#e = b
+#s = c
+
+#Calculations
+lives_level = print("a", * lives)
+#energy_level = energy*e
+#shield_level = shield*s
+
+#Results
+print("Lives: ", lives_level)
+print("")
+#print("Energy: ", energy_level)
+#print("")
+#print("Shield: ", shield_level)
