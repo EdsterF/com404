8054ff084d9a412c3d968d0efec2172055e07a70
EdsterF <mr.eduardo.franco@gmail.com>
2019-10-01 14:08:03 +0000

    elif3

--- 1-basics/3-decision/3-if-elif-else/bot.py
+++ 1-basics/3-decision/3-if-elif-else/bot.py
@@ -20,3 +20,21 @@ else:
 
 #Final Statement
 print("Shall we try a different direction now?")
+
+#User input
+paint_direction = input()
+
+#conditions
+if (paint_direction == "up"):
+    print("I am painting in the upward direction now!")
+elif(paint_direction == "down"):
+    print("I am painting in the downward direction now!")
+elif (paint_direction == "left"):
+    print("I am painting in the left direction now!")
+elif (paint_direction == "right"):
+    print("I am painting in the right direction now!")
+else:
+    print("Can't achieve given direction")
+
+#Final Statement
+print("That's enough painting for me now")
