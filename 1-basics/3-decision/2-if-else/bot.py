4ea0094ca12ada3efdcdc2f6f6144d1659f2b973
EdsterF <mr.eduardo.franco@gmail.com>
2019-10-01 13:55:34 +0000

    elif

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,9 @@
+print("please enter the activity performed")
+activity = input()
+
+if (activity == "calculate"):
+    print("performing calculations")
+else:
+    print("performing activity...")
+
+print("Activity completed!")
