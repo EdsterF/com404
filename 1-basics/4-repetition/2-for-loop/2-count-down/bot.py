f5f71e32fd244a1c92dbe254ad70968ca8d387d6
EdsterF <mr.eduardo.franco@gmail.com>
2019-10-09 09:39:57 +0000

    Added code to demonstrate counting down with a for loop.

---
+++ 1-basics/4-repetition/2-for-loop/2-count-down/bot.py
@@ -0,0 +1,23 @@
+#Intro message
+print("How far are we from the cave?")
+
+#User input
+cave_distance = int(input())
+print("")
+
+#for loop
+for count in range (cave_distance, 0, -1):
+    #Range explained, based on a count-down
+    #cave_distance = where the count starts (based on user input)
+    #0 = where the count ends
+    #1 = step or difference between each number
+
+    print(str(count) + " steps remaining")
+    #count explained
+    #count is a variable inside the loop:
+    #this contains the "total" for every time the loop goes around
+    #in this case it subtracts one from the total every time
+
+#Final message
+print("")
+print("We have reached the cave!")
