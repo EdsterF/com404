dc8b462a4d32f573ccfac969439f08c1f58f7795
EdsterF <mr.eduardo.franco@gmail.com>
2019-10-15 13:44:23 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ 1-basics/5-functions/1-simple/bot.py
@@ -0,0 +1,12 @@
+#Function
+def listen():
+    print("What did I hear?")
+    sound = input()
+    
+    print("")
+    print("That was a loud", sound, "!")
+
+#Call to the function
+listen ()
+listen ()
+
